5d26e2bf9ef0c175b468eeeccf30def8c11b5817
cloudatlasx <eclipsexmail@gmail.com>
2022-06-08 17:14:39 +0800

    Initial commit

---
+++ java-sample/src/test/java/org/learn/galaxy/javasample/JavaSampleApplicationTests.java
@@ -0,0 +1,14 @@
+package org.learn.galaxy.javasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("test");
+    }
+
+}
