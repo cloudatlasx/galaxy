019aaf1a579cceccb16007f92bebf93c196b9915
cloudatlasx <eclipsexmail@gmail.com>
2023-11-06 16:49:30 +0800

    【ADD】 skywalking

---
+++ skywalking-demo/src/test/java/onem/learn/SkywalkingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package onem.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkywalkingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
